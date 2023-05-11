e37053b891d00b3e7c8253ce00ed82222f3c0abb
rayyh <rayyounghong@gmail.com>
2023-05-11 22:54:01 +0800

    feat: security

--- discovery-server/src/main/java/com/rayyounghong/sbms/discoveryserver/config/SecurityConfig.java
+++ discovery-server/src/main/java/com/rayyounghong/sbms/discoveryserver/config/SecurityConfig.java
@@ -1,15 +1,37 @@
 package com.rayyounghong.sbms.discoveryserver.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+  @Value("${eureka.username}")
+  private String username;
+
+  @Value("${eureka.password}")
+  private String password;
+
+  @Bean
+  @SuppressWarnings("deprecation")
+  public InMemoryUserDetailsManager userDetailsService() {
+    UserDetails user = User.withDefaultPasswordEncoder()
+        .username(username)
+        .password(password)
+        .roles("USER")
+        .build();
+    return new InMemoryUserDetailsManager(user);
+  }
+
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-    httpSecurity.csrf().ignoringAntMatchers("/eureka/**");
+    httpSecurity.csrf().disable().authorizeRequests().anyRequest().authenticated().and()
+        .httpBasic();
     return httpSecurity.build();
   }
 }
