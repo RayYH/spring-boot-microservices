298c99ad05894b84307ca393ea5bde4fa1d63b1f
rayyh <rayyounghong@gmail.com>
2023-05-12 16:47:01 +0800

    feat: update

---
+++ notification-service/src/main/java/com/rayyounghong/sbms/notificationservice/listener/OrderPlacedEventListener.java
@@ -0,0 +1,16 @@
+package com.rayyounghong.sbms.notificationservice.listener;
+
+import com.rayyounghong.sbms.notificationservice.event.OrderPlacedEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class OrderPlacedEventListener {
+
+  @KafkaListener(topics = "notificationTopic")
+  public void listen(OrderPlacedEvent orderPlacedEvent) {
+    log.info("Notification Service - OrderPlacedEvent consumed: {}", orderPlacedEvent);
+  }
+}
